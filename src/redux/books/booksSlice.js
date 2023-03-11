e14e9a9d8f78c2fa7ce17764589cbe0644bc9df7
Osman Alhassan <oalhassan847@gmail.com>
2023-03-11 23:49:12 +0000

    Add books display asyncthunk

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,11 +1,32 @@
 import {
   createSlice,
+  createAsyncThunk,
 } from '@reduxjs/toolkit';
 import books from '../../components/books';
 
 const initialState = {
   books,
 };
+
+const booksURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/YTkkGNjFzdVaLfwvsjLv/books';
+
+const booksDisplay = createAsyncThunk(
+  'bookstore/books',
+  async () => {
+    const response = await fetch(booksURL);
+    const data = response.json;
+    const Bookss = [];
+    Object.keys(data).forEach((ind) => {
+      const each = {
+        id: ind,
+        title: data[ind][0].title,
+        author: data[ind][0].author,
+        category: data[ind][0].category,
+      };
+      Bookss.push(each);
+    });
+    return Bookss;
+  });
 
 const booksSlice = createSlice({
   name: 'book',
